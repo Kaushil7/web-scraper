765553744c13a3bcc0474e556cbbf81fccb68954
Kaushil <kaushil.nagrale@gmail.com>
2023-08-29 19:51:41 +0530

    Second Commit

---
+++ College_scraper.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Define the URL you want to scrape
+url = "https://www.ymgrad.com/search/United%20States"
+
+# Send an HTTP GET request to the URL
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
+    
+    # Find the outer-most div using its class name
+    outer_div = soup.find("h3", class_="UniversityCardNew__university_name___")
+    print(outer_div)
+    
+
+    
